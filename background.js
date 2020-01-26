893f2029a6fd924120ff11ad12dbb14fc0ea03b1
Austin-Montes <a0140604@ufl.edu>
2020-01-26 00:10:28 -0500

    created extension popup

---
+++ background.js
@@ -0,0 +1,15 @@
+
+/*chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.contentScriptQuery == 'fetchUrl') {
+        // WARNING: SECURITY PROBLEM - a malicious web page may abuse
+        // the message handler to get access to arbitrary cross-origin
+        // resources.
+        fetch(request.url)
+            .then(response => response.text())
+            .then(text => sendResponse(text))
+            .catch(error => console.log("error"))
+        return true;  // Will respond asynchronously.
+      }
+});
+*/

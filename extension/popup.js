1727c3ddbca44b2b38916be91378050eab074d4e
mmills9269 <mmills4@ufl.edu>
2020-03-28 17:34:00 -0400

    Initial loading animation code added

--- extension/popup.js
+++ extension/popup.js
@@ -1,14 +1,13 @@
-
 // JS function that can perform get requestions
 var HttpClient = function() {
     this.get = function(aUrl, aCallback) {
         var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() { 
+        anHttpRequest.onreadystatechange = function() {
             if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
                 aCallback(anHttpRequest.responseText);
         }
 
-        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.open( "GET", aUrl, true );
         anHttpRequest.send( null );
     }
 }
@@ -26,7 +25,9 @@ chrome.tabs.executeScript( {
     var client = new HttpClient();
     client.get(query, function(response) {
         document.getElementById("output").innerHTML = response + " " + query;
+
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("output").style.display = "block";
     });
 
 });
-
